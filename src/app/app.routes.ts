a6921003b76dc3db337dd65ca03207417a3547f7
Diptesh Karmakar <dipteshkarmakar007@gmail.com>
2025-02-03 12:20:20 +0530

    new all team and player page with serach

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,18 @@ import { TeamComponent } from './myComponents/team/team.component';
 import { ScheduleComponent } from './myComponents/schedule/schedule.component';
 import { ChooseTeamComponent } from './myComponents/choose-team/choose-team.component';
 import { ChoosePlayersComponent } from './myComponents/choose-players/choose-players.component';
-
+import { PlayersComponent } from './myComponents/all-player/all-player.component';
+import { AllTeamsComponent } from './myComponents/all-teams/all-teams.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: SignupComponent },
-  { path: 'team-page/:playerId', component: TeamPageComponent },  // Keeping playerId for dynamic routing
-  { path: 'team/:teamId', component: TeamComponent },             // Dynamic teamId
-  { path: 'match/:matchId', component: MatchComponent },          // Dynamic matchId
+  { path: 'all-players', component: PlayersComponent },
+  { path: 'all-teams', component: AllTeamsComponent },
+  { path: 'team-page/:playerId', component: TeamPageComponent }, // Keeping playerId for dynamic routing
+  { path: 'team/:teamId', component: TeamComponent }, // Dynamic teamId
+  { path: 'match/:matchId', component: MatchComponent }, // Dynamic matchId
   { path: 'schedule', component: ScheduleComponent },
   { path: 'choose-team', component: ChooseTeamComponent },
-  {path: 'choose-player', component: ChoosePlayersComponent},
-  { path: '**', redirectTo: '', pathMatch: 'full' }               // Fallback route
+  { path: 'choose-player', component: ChoosePlayersComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Fallback route
 ];
