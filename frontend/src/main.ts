5399c75c54ff12881a8f81eb1c105ac4d7761436
Kaustav Das <kaustavdas0228@gmail.com>
2025-02-06 01:48:33 +0530

    deploy

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { RouterModule } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-// import { environment } from '../enviourment';
 import { routes } from './app/app.routes';
 import { provideToastr, ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,7 +27,5 @@ bootstrapApplication(AppComponent, {
         easeTime: 300,
       }),       
     ),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
   ],
 }).catch((err) => console.error(err));
